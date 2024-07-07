b2e45b33b45f3f4db81243616fba89379585b7bb
white_night <yash91471@gmail.com>
2024-07-07 13:10:24 +0530

    Time interval set

--- src/App.js
+++ src/App.js
@@ -5,29 +5,38 @@ import { Navbar } from './Components/Navbar/Navbar';
 import { Hero } from './Components/Hero/Hero';
 
 function App() {
-  let heroData = [
-    {text1:"Live Life", text2:"With Desires"},
-    {text1:"Drive to" , text2:"Your Dreams"},
-    {text1:"Feel Speed",text2:"Feel Life"}
-  ]
-  const[heroCount,setHeroCount] = useState(0);
-  const[playStatus,setPlayStatus] = useState(false);
+  const heroData = [
+    {text1: "Live Life", text2: "With Desires"},
+    {text1: "Drive to", text2: "Your Dreams"},
+    {text1: "Feel Speed", text2: "Feel Life"}
+  ];
 
-  useEffect(() =>{
-    setHeroCount((count)=>{return count===2?0:count++;});
-  },[])
+  const [heroCount, setHeroCount] = useState(0);
+  const [playStatus, setPlayStatus] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setHeroCount((count) => (count === 2 ? 0 : count + 1));
+    }, 3000);
+
+    return () => clearInterval(interval); 
+  }, []);
+
   return (
     <div className="App">
-      <Background playStatus={playStatus} heroCount={heroCount}/>
+      <Background playStatus={playStatus} heroCount={heroCount} />
       <Navbar />
-      <Hero setHeroCount= {setHeroCount}
-      heroCount={heroCount}
-      setPlayStatus = {setPlayStatus} 
-      playStatus={playStatus}
-      heroData={heroData[heroCount]}
+      <Hero
+        setHeroCount={setHeroCount}
+        heroCount={heroCount}
+        setPlayStatus={setPlayStatus}
+        playStatus={playStatus}
+        heroData={heroData[heroCount]}
       />
     </div>
   );
 }
+
+
 
 export default App;
